ae007c9229d5f137bcc0a73d675346ab054bc859
Pericles <slouis482@gmail.com>
2025-10-31 16:54:34 -0400

    se creo la pantalla de checkaut

--- src/app/pages/landing/components/checkout/checkout.ts
+++ src/app/pages/landing/components/checkout/checkout.ts
@@ -1,17 +1,36 @@
+import { AuthService } from '@/services/auth.service';
+import { ShoppingCartService } from '@/services/shoppingCard.service';
 import { Component } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { Breadcrumb } from 'primeng/breadcrumb';
+import { InputTextModule } from 'primeng/inputtext';
+import { TextareaModule } from 'primeng/textarea';
 
 @Component({
   selector: 'app-checkout',
-  imports: [Breadcrumb],
+  imports: [Breadcrumb, InputTextModule, TextareaModule],
   templateUrl: './checkout.html',
   styleUrl: './checkout.scss'
 })
 export class Checkout {
+  user: any;
+  products: any[] = [];
   items: MenuItem[] = [
     { label: 'Hogar', routerLink: '/landing' },
     { label: 'Tienda', routerLink: '/shop' },
     { label: 'verificación ' }
   ];
+
+  constructor(
+    private authService: AuthService,
+    private shoppingCartService: ShoppingCartService
+  ) {}
+
+  ngOnInit() {
+    this.shoppingCartService.cart_products$.subscribe((items) => {
+      this.products = items;
+    });
+
+    this.user = this.authService.decodeToken().user;
+  }
 }
