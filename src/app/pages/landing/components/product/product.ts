e5c89896f7f932c99c622e77ca0035b53545e316
DESKTOP-MJLISDU\La Pampara <slouis482@gmail.com>
2026-01-23 23:34:32 -0400

    cambio de color en product, shop product, landing page

--- src/app/pages/landing/components/product/product.ts
+++ src/app/pages/landing/components/product/product.ts
@@ -93,7 +93,7 @@ export class ProductComponent {
 
     return {
       label: 'Disponible',
-      bgClass: 'bg-surface-700',
+      bgClass: 'bg-green-600',
       textClass: 'text-green-100'
     };
   }
